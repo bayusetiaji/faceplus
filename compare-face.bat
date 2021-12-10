959f3fb77a6c5e790e24a17eff8017d1f3e4f25e
Bayu Setiaji <bayusetiaji@amikom.ac.id>
2021-12-10 14:22:11 +0700

    Update compare-face.bat

--- compare-face.bat
+++ compare-face.bat
@@ -1,6 +1,6 @@
 curl -X POST "https://api-us.faceplusplus.com/facepp/v3/compare"^
- -F "api_key=gC9CHnYWsww_i9-gDQyiBv4yv0Tijr0o"^
- -F "api_secret=JuWcBZOeS8pyCiNJZgFLwK2j7Ftzo7wK"^
+ -F "api_key=<API Key>"^
+ -F "api_secret=<API Secret>"^
  -F "image_url1=https://pict-a.sindonews.net/dyn/620/jatim/news/2019/03/02/1/7867/nama-soekarno-diabadikan-jadi-nama-tempat-dan-perangko-di-6-negara-ini-hew.jpg"^
  -F "image_url2=https://ewr1.vultrobjects.com/suarapapuaweb/2019/10/bung-hatta.jpg"^
   > result.json 
