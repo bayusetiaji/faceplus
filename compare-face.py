eae95990637ae4c9f554c9320795fc1c6e97480c
Bayu Setiaji <bayusetiaji@amikom.ac.id>
2021-12-10 14:20:21 +0700

    Add files via upload

---
+++ compare-face.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+url = 'https://api-us.faceplusplus.com/facepp/v3/compare' # API URL
+api_k = 'gC9CHnYWsww_i9-gDQyiBv4yv0Tijr0o' # API Key
+api_s = 'JuWcBZOeS8pyCiNJZgFLwK2j7Ftzo7wK' # API Secret
+
+img1 = 'https://pict-a.sindonews.net/dyn/620/jatim/news/2019/03/02/1/7867/nama-soekarno-diabadikan-jadi-nama-tempat-dan-perangko-di-6-negara-ini-hew.jpg'
+img2 = 'https://ewr1.vultrobjects.com/suarapapuaweb/2019/10/bung-hatta.jpg'
+
+# setting parameter
+payload = dict(api_key = api_k, api_secret = api_s, image_url1 = img1, image_url2 = img2)
+
+# request
+response = requests.post(url, data = payload)
+
+# result
+result = response.text
+
+print(json.loads(result)['confidence'])
